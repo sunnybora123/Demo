b6f2d0134e7ab9a68edb13568d4f9e02eeba7ecb
sunnybora123 <sunnybora986@gmail.com>
2024-05-17 08:13:16 +0530

    commit initial

---
+++ src/main/java/com/onlyjavatech/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.onlyjavatech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	//This is it
+
+}
